8963ff521c32345f29224d68ae1ebf0209751208
ubuntu <ubuntu@localhost.localdomain>
2021-07-13 15:50:54 +0800

    testing

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@ currentTime=`date "+%Y-%m-%d %H:%M:%S"`
 
 python2.7 /opt/Web/main_up.py & >> /opt/Web/Logs/start_test.log
 echo $currentTime >> /opt/Web/Logs/start_test.log
+echo "Start Web Server" >> /opt/Web/Logs/start_test.log
 /bin/bash
